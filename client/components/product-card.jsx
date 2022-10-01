5fe89142e33e9be546e2730a023e34947feec368
Konstantin Savusia <savconstantine@gmail.com>
2022-10-01 07:17:01 +0600

    Contains product cards. Card with class(.card) picture(.card__image) price(.card__price) (in current currency) currency(.currency) product name(.card__title) if this product is in the basket in the amount of n pieces, output the number of products in the basket(.cart__product-amount), if not, do not output anything to the add button

--- client/components/product-card.jsx
+++ client/components/product-card.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="card w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img
-          className="p-8 rounded-t-lg object-cover w-full max-h-72"
+          className="card__image p-8 rounded-t-lg object-cover w-full max-h-72"
           src={product.image}
           alt="product"
         />
       </a>
       <div className="px-5 pb-5">
         <a href="#">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="card__title text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
             {product.title}
           </h5>
-          <p className=" font-normal text-gray-700 dark:text-gray-400">{product.description}</p>
+          <p className="font-normal text-gray-700 dark:text-gray-400">{product.description}</p>
           <span className="mb-3 text-xs font-normal text-gray-700 dark:text-gray-400">
             {product.id}
           </span>
@@ -76,13 +76,15 @@ const ProductCard = ({ product }) => {
           </span>
         </div>
         <div className="flex justify-between items-center">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">{product.price}</span>
-          <a
-            href="#"
+          <span className="card__price text-3xl font-bold text-gray-900 dark:text-white">
+            {product.price}
+          </span>
+          <button
+            type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add to cart
-          </a>
+          </button>
         </div>
       </div>
     </div>
