8b68e90da2d8813707f6a52411e01f7dc5673f15
Konstantin Savusia <savconstantine@gmail.com>
2022-10-07 07:30:09 +0600

    api sort products

--- server/server.js
+++ server/server.js
@@ -33,12 +33,38 @@ server.get('/', (req, res) => {
   `)
 })
 
-server.get('/api/v1/products', async (req, res) => {
-  const productsArray = await readFile(`${__dirname}/data/products.json`, 'utf-8')
+const getProducts = () => {
+  return readFile(`${__dirname}/data/products.json`, 'utf-8')
     .then((data) => JSON.parse(data))
     .catch((err) => console.log(err))
+}
 
+const sortProductsList = (products, sort, order) => {
+  switch (sort) {
+    case 'price':
+      return products.sort((a, b) => {
+        return order === 'asc' ? a.price - b.price : b.price - a.price
+      })
+    case 'name':
+      return products.sort((a, b) => {
+        return order === 'asc' ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title)
+      })
+    default:
+      return products
+  }
+}
+
+server.get('/api/v1/products', async (req, res) => {
+  const productsArray = await getProducts()
   res.json(productsArray.slice(0, 50))
+})
+
+server.post('/api/v1/products/sort', async (req, res) => {
+  const productsArray = await getProducts()
+  const { sort, order } = req.body
+  const sortedProducts = sortProductsList(productsArray, sort, order)
+
+  res.json(sortedProducts)
 })
 
 const exchangeRateUrl = 'https://api.exchangerate.host/latest?base=USD&symbols=USD,EUR,CAD'
