080edb5ceab41db32023b8e7d4cb03875db189d9
Konstantin Savusia <savconstantine@gmail.com>
2022-10-11 09:17:33 +0600

    send log to server

--- client/redux/middleware/logging.js
+++ client/redux/middleware/logging.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 import { SET_CURRENT_CURRENCY } from '../reducers/settings'
 import { ADD_TO_CART, REMOVE_FROM_CART, INCREASE_AMOUNT, DECREASE_AMOUNT } from '../reducers/cart'
 import { SET_SORT_INFO } from '../reducers/products'
@@ -16,50 +18,54 @@ const LoggingMiddleware = () => {
             date.getMonth() + 1
           }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
         }
+
+        const setLogs = (logStr) => {
+          const uniqueId = +new Date()
+          axios
+            .post('/api/v1/logs', { id: uniqueId, string: logStr })
+            .then(({ data }) => data)
+            .then((list) =>
+              dispatch({
+                type: LOG_UPDATE,
+                payload: list
+              })
+            )
+            .catch((err) => console.log(err))
+        }
         switch (action.type) {
           case SET_CURRENT_CURRENCY:
             {
               const { currentCurrency } = getState().settings
               const newCurrency = action.payload
-              dispatch({
-                type: LOG_UPDATE,
-                payload: `${getDate()} - Currency changed from ${currentCurrency} to ${newCurrency}`
-              })
+              const logString = `${getDate()} - Currency changed from ${currentCurrency} to ${newCurrency}`
+              setLogs(logString)
             }
             break
           case ADD_TO_CART:
           case INCREASE_AMOUNT: {
             const item = action.payload.product
-            dispatch({
-              type: LOG_UPDATE,
-              payload: `${getDate()} - Add item ${item.title} to the cart`
-            })
+            const logString = `${getDate()} - Add item ${item.title} to the cart`
+            setLogs(logString)
             break
           }
           case REMOVE_FROM_CART:
           case DECREASE_AMOUNT: {
             const item = action.payload.product
-            dispatch({
-              type: LOG_UPDATE,
-              payload: `${getDate()} - Remove item ${item.title} from the cart`
-            })
+            const logString = `${getDate()} - Remove item ${item.title} from the cart`
+            setLogs(logString)
             break
           }
           case SET_SORT_INFO: {
             const { sort, order } = action.payload
-            dispatch({
-              type: LOG_UPDATE,
-              payload: `${getDate()} - Sort products list by ${sort} in ${order} order`
-            })
+            const logString = `${getDate()} - Sort products list by ${sort} in ${order} order`
+            setLogs(logString)
             break
           }
           case '@@router/LOCATION_CHANGE':
             {
               const url = action.payload.location.pathname
-              dispatch({
-                type: LOG_UPDATE,
-                payload: `${getDate()} - navigate to ${url} page`
-              })
+              const logString = `${getDate()} - navigate to ${url} page`
+              setLogs(logString)
             }
             break
           default:
