06f25d503c2e070db0935720e42a5a0a21f42845
Konstantin Savusia <savconstantine@gmail.com>
2022-10-11 09:18:10 +0600

    add footer and put log link to it

---
+++ client/components/footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = (props) => {
+  return (
+    <footer className="fixed bottom-0 w-full p-4 bg-white rounded-lg shadow  dark:bg-gray-800">
+      <div className="max-w-screen-lg mx-auto md:flex md:items-center md:justify-between md:p-6">
+        <span className=" text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <Link to="/" className="hover:underline">
+            {props.title}™
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link to="/logs" href="#" className="mr-4 hover:underline md:mr-6 ">
+              Logs
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
+
+Footer.propTypes = {}
+
+export default React.memo(Footer)
--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Head from './head'
 import Header from './header'
+import Footer from './footer'
 import Products from './products'
 
 const Main = () => {
+  const title = 'BrandName'
   return (
     <>
       <Head title="Main" />
-      <Header title="BrandName" />
+      <Header title={title} />
       <Products />
+      <Footer title={title} />
     </>
   )
 }
