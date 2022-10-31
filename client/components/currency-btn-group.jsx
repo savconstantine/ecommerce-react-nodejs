a910a2702750334e2e97d8c0fef79aa1c51ee83a
Konstantin Savusia <savconstantine@gmail.com>
2022-11-01 04:22:17 +0600

    update currency btn-group

--- client/components/currency-btn-group.jsx
+++ client/components/currency-btn-group.jsx
@@ -10,7 +10,7 @@ const CurrencyBtnGroup = () => {
   }, [])
 
   return (
-    <div className="inline-flex rounded-md shadow-sm" role="group">
+    <div className="inline-flex rounded-md shadow-sm md:ml-2" role="group">
       <button
         type="button"
         className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
--- client/components/products.jsx
+++ client/components/products.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getProductsFromServer } from '../redux/reducers/products'
 import ProductCard from './product-card'
 import ProductsSortDropdown from './products-sort-dropdown'
+import CurrencyBtnGroup from './currency-btn-group'
 
 const Products = () => {
   const productList = useSelector((state) => state.products.list)
@@ -20,6 +21,7 @@ const Products = () => {
     <>
       <div className="flex max-w-screen-lg mx-auto justify-center pt-10">
         <ProductsSortDropdown />
+        <CurrencyBtnGroup />
       </div>
       <div className="flex flex-wrap gap-2 max-w-screen-lg mx-auto justify-center pt-10">
         {productListArray.map((product) => (
